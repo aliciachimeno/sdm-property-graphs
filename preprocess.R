d44429af9f8a37507d34d71b0f228e54c6e0da9c
Alicia <aliciachimeno9@gmail.com>
2024-03-14 19:58:03 +0100

    update papers

--- preprocess.R
+++ preprocess.R
@@ -4,21 +4,32 @@ setwd("/Users/ali/Downloads/dblp-to-csv-master")
 library(skimr)
 library(ggplot2)
 library(tidyverse)
+library(dplyr)
 
 ## authors, papers, conference, editions, volume, journal, 
 
-df_authors<-read.csv("dblp_author.csv",sep=";",row.names =NULL, encoding = "latin1")
-df_authors
+dblp_article<-read.csv("dblp_article.csv",sep=";",row.names =NULL, encoding = "latin1",nrows=100000)
+dblp_article
+dblp_article_header<-read.csv("dblp_article_header.csv",sep=";",row.names =NULL, encoding = "latin1")
+header<-names(dblp_article_header)
+colnames(dblp_article)<-header
+colnames(dblp_article)
 
-df_books_header<-read.csv("dblp_book_header.csv",sep=";",row.names =NULL, encoding = "latin1")
-header<-names(df_books_header)
 
-df_books<-read.csv("dblp_book.csv",sep=";",row.names =NULL, encoding = "latin1")
-df_books
-colnames(df_books)<-header
-df_books
-skim(df_books)
-summary(df_books$journal.string)
+# Assuming df is your dataframe
+dblp_article <- na_if(dblp_article, "")
+skim(dblp_article)
 
-df_books$journal.string<-as.factor(df_books$journal.string)
-names(df_books)
+#summary(df_books$journal.string)
+#df_books$journal.string<-as.factor(df_books$journal.string)
+
+
+missing_prop <- colMeans(is.na(dblp_article))
+threshold <- 0.8  # Adjust as needed
+columns_to_remove <- names(dblp_article)[missing_prop > threshold]
+dblp_article <- dblp_article[, !names(dblp_article) %in% columns_to_remove]
+colnames(dblp_article)
+dblp_article[,1:2]
+
+
+

6849c64a030997c61831b6646f376c51553f217e
Alicia <aliciachimeno9@gmail.com>
2024-03-16 13:34:53 +0100

    up

--- preprocess.R
+++ preprocess.R
@@ -6,9 +6,6 @@ library(ggplot2)
 library(tidyverse)
 library(dplyr)
 
-
-### graphs modeling 
-
 # paper -> journal
 
 # load
@@ -37,18 +34,18 @@ dblp_article$pages.string # !!abanico de pages, no num of pages
 dblp_article$volume.string
 dblp_article[,c(1,2,4,5,9,10,12,13)]
 
-# paper -> conference
-
-# dades dels papers
+# paper -> conference 
+#inproceedings: dades dels papers
 dblp_inproceedings<-read.csv("dblp_inproceedings.csv",sep=";",row.names =NULL, encoding = "latin1",nrows=100000)
 dblp_inproceedings_header<-read.csv("dblp_inproceedings_header.csv",sep=";",row.names =NULL, encoding = "latin1")
 header_3<-names(dblp_inproceedings_header)
 colnames(dblp_inproceedings)<-header_3
 
 names(dblp_inproceedings)
+dblp_inproceedings$author.string.. #author
 dblp_inproceedings$title.string # titul dels papers
-dblp_inproceedings$crossref.string..
-dblp_inproceedings_db<-dblp_inproceedings[,c(9,25)]
+dblp_inproceedings$crossref.string.. # key pel join
+dblp_inproceedings_db<-dblp_inproceedings[,c(2,9,25)]
 
 # dades de les conferences: 
 # preprocess
@@ -66,5 +63,63 @@ dblp_proceedings_db<-dblp_proceedings[,c(15,29,32)]
 
 # join
 names(dblp_proceedings_db)
-inner_join_df <- inner_join(dblp_proceedings_db, dblp_inproceedings_db, by = c("key.string" = "crossref.string.."))
-inner_join_df
+left_join_df <- left_join(dblp_inproceedings_db, dblp_proceedings_db, by = c("crossref.string.."="key.string"))
+left_join_df$title.string.y
+
+left_join_df$title.string.y
+
+#################################################
+
+conference_split <- strsplit(left_join_df$title.string.y, ", ")
+conference_split[[2]] # exaple of entry
+conference_name <- sapply(conference_split, function(x) paste(x[1], collapse = ", "))
+detect_location <- function(entry) {
+  location <- entry[nchar(entry) < 20 & !grepl("\\d", entry)] # Select entries shorter than 20 characters and without any number
+  location <- paste(location, collapse = ", ") # Combine into a single string
+  return(location)
+}
+locations <- sapply(conference_split, detect_location)
+conference_year <- as.numeric(sub(".*\\b(\\d{4})\\b.*", "\\1", left_join_df$title.string.y))
+
+
+conference_title <- sub(".*?((?:Conference|Workshop).*?$)", "\\1", conference_df$conference_name)
+conference_edition <- extract_ordinals(conference_name)
+
+
+extract_ordinals <- function(names_list) {
+  # Regular expression pattern to match ordinal numbers up to "30th"
+  ordinal_pattern <- "\\b(?:1st|2nd|3rd|(?:[4-9]|1[0-9]|2[0-9]|30)th|First|Second|Third|Fourth|Fifth|Sixth|Seventh|Eighth|Ninth|Tenth|Eleventh|Twelfth|Thirteenth|Fourteenth|Fifteenth)\\b"
+  
+  # Extract ordinal numbers from each entry
+  extracted_ordinals <- regmatches(names_list, gregexpr(ordinal_pattern, names_list))
+  
+  # Create a vector to store the results
+  result <- character(length(names_list))
+  
+  # Loop through each entry to assign ordinal numbers or NA
+  for (i in seq_along(names_list)) {
+    if (length(extracted_ordinals[[i]]) > 0) {
+      result[i] <- extracted_ordinals[[i]]
+    } else {
+      result[i] <- NA
+    }
+  }
+  
+  return(result)
+}
+
+
+conference_title <- sub(".*?((?:Conference|Workshop).*?$)", "\\1", conference_df$conference_name)
+conference_edition <- extract_ordinals(conference_name)
+
+
+  
+# Create a data frame
+conference_df <- data.frame(
+  conference_name=conference_name,
+  conference_title = conference_title,
+  conference_edition=conference_edition,
+  location = locations,
+  year=conference_year
+)
+

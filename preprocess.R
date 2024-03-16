cea86410e0ab0fe11658bab6ffa863d4e012a3d0
Alicia <aliciachimeno9@gmail.com>
2024-03-16 17:40:07 +0100

    update final

--- preprocess.R
+++ preprocess.R
@@ -1,30 +1,21 @@
 getwd()
 setwd("/Users/ali/Downloads/dblp-to-csv-master")
 
+
 library(skimr)
 library(ggplot2)
 library(tidyverse)
 library(dplyr)
 
+#########################################################################################################
 # paper -> journal
 
-# load
-dblp_article<-read.csv("dblp_article.csv",sep=";",row.names =NULL, encoding = "latin1",nrows=100000)
-# load header
-dblp_article_header<-read.csv("dblp_article_header.csv",sep=";",row.names =NULL, encoding = "latin1")
-header<-names(dblp_article_header)
-colnames(dblp_article)<-header
-#preprocess
-dblp_article <- na_if(dblp_article, "")
-skim(dblp_article)
-missing_prop <- colMeans(is.na(dblp_article))
-threshold <- 0.8  
-columns_to_remove <- names(dblp_article)[missing_prop > threshold]
-dblp_article <- dblp_article[, !names(dblp_article) %in% columns_to_remove]
-colnames(dblp_article)
+# loading
+dblp_article<-read.csv("dblp_article.csv",sep=";",row.names =NULL, encoding = "latin1",nrows=100000) # load
+dblp_article_header<-read.csv("dblp_article_header.csv",sep=";",row.names =NULL, encoding = "latin1") # load header
+colnames(dblp_article)<-names(dblp_article_header) # put the header
 
-
-## DATA EXPLORATION
+## data exploration
 ## only want to keep: paper, authors, title, journal, volume, year, doi,
 names(dblp_article)
 dblp_article$article.ID # id
@@ -32,37 +23,65 @@ dblp_article$author.string.. # string de autors
 dblp_article$ee.string.. # doi number
 dblp_article$pages.string # !!abanico de pages, no num of pages
 dblp_article$volume.string
-dblp_article[,c(1,2,4,5,9,10,12,13)]
+dblp_article[,c(1,2,13,16,24,30,34,35)]
 
+
+#########################################################################################################
 # paper -> conference 
-#inproceedings: dades dels papers
+###################### inproceedings: dades dels PAPERS
+
 dblp_inproceedings<-read.csv("dblp_inproceedings.csv",sep=";",row.names =NULL, encoding = "latin1",nrows=100000)
 dblp_inproceedings_header<-read.csv("dblp_inproceedings_header.csv",sep=";",row.names =NULL, encoding = "latin1")
-header_3<-names(dblp_inproceedings_header)
-colnames(dblp_inproceedings)<-header_3
+colnames(dblp_inproceedings)<-names(dblp_inproceedings_header)
 
+#explore
 names(dblp_inproceedings)
 dblp_inproceedings$author.string.. #author
 dblp_inproceedings$title.string # titul dels papers
-dblp_inproceedings$crossref.string.. # key pel join
+dblp_inproceedings$crossref.string.. # key 
 
-# dades de les conferences: 
+### relation: id_papers with authors
+dblp_inproceedings$author.string.. #author
+dblp_inproceedings$title.string # titul dels papers
+dblp_inproceedings$inproceedings.ID # id dels papers
+
+split_authors <- strsplit(dblp_inproceedings$author.string.., "\\|") # split els authors
+
+authors_papersID <- data.frame(
+  id_paper = rep(dblp_inproceedings$inproceedings.ID, sapply(split_authors, length)),
+  author = unlist(split_authors)
+)
+
+authors_papersID
+#write.csv(authors_papersID, "authors_idpapers.csv", row.names = FALSE)
+
+
+### relation id_paper with paper
+
+idpaper_paper<- data.frame(
+  id_paper=dblp_inproceedings$inproceedings.ID,
+  paper_title=dblp_inproceedings$title.string
+)
+
+## try to join paper and author ->left join
+#l_j<-left_join(idpaper_paper,authors_papersID,by=c(c("id_paper"="id_paper"))) # ok works
+
+###################### proceedings: dades de les CONFERENCES: 
 # preprocess
 dblp_proceedings<-read.csv("dblp_proceedings.csv",sep=";",row.names =NULL, encoding = "latin1",nrows=100000)
 dblp_proceedings_header<-read.csv("dblp_proceedings_header.csv",sep=";",row.names =NULL, encoding = "latin1")
-header_4<-names(dblp_proceedings_header)
-colnames(dblp_proceedings)<-header_4
+colnames(dblp_proceedings)<-names(dblp_proceedings_header)
 
 #explore
 colnames(dblp_proceedings)
-dblp_proceedings$title.string # conference / workshop name 
-dblp_proceedings$key.string
+dblp_proceedings$title.string # conference / workshop complete 
+dblp_proceedings$booktitle.string # conference / workshop name 
+dblp_proceedings$key.string # key
 
-
-#################################################
+# ------------------------------------------------------------------------------------------
 
 conference_split <- strsplit(dblp_proceedings$title.string, ", ")
-conference_split[[945]] # exaple of entry
+conference_split[[945]] # example of entry
 conference_name <- sapply(conference_split, function(x) paste(x[1], collapse = ", "))
 detect_location <- function(entry) {
   location <- entry[nchar(entry) < 20 & !grepl("\\d", entry)] # Select entries shorter than 20 characters and without any number
@@ -74,8 +93,6 @@ conference_year <- as.numeric(sub(".*\\b(\\d{4})\\b.*", "\\1", dblp_proceedings$
 
 
 conference_title <- sub(".*?((?:Conference|Workshop).*?$)", "\\1", conference_name)
-conference_edition <- extract_ordinals(conference_name)
-
 
 extract_ordinals <- function(names_list) {
   # Regular expression pattern to match ordinal numbers up to "30th"
@@ -98,11 +115,12 @@ extract_ordinals <- function(names_list) {
   
   return(result)
 }
-
+conference_edition <- extract_ordinals(conference_name)
 
   
 conference_df <- data.frame(
   conference_complete=dblp_proceedings$title.string,
+  acronym=dblp_proceedings$booktitle.string,
   ref=dblp_proceedings$key.string,
   conference_name=conference_name,
   conference_title = conference_title,
@@ -115,7 +133,7 @@ conference_df <- data.frame(
 #conference_df_complete <- na.omit(conference_df)
 
 
-# join
+# join exmple
 dblp_inproceedings_db<-dblp_inproceedings[,c(2,9,25)]
 names(conference_df)
 left_join_df <- left_join(dblp_inproceedings_db, conference_df, by = c("crossref.string.."="ref"))
